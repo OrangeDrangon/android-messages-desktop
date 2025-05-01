cb7485906bb82b93a61cb8bffd6bc88e29d4329f
Restyled.io <commits@restyled.io>
2025-05-01 13:00:47 +0000

    Restyled by clang-format

--- electron-builder.js
+++ electron-builder.js
@@ -3,26 +3,27 @@ export default {
   artifactName: "${productName}-v${version}-${os}-${arch}.${ext}",
   productName: "Android Messages",
   copyright: "Copyright 2025 Kyle Rosenberg",
-  files: ["app/**/*", "resources/**/*"],
+  files: [ "app/**/*", "resources/**/*" ],
   directories: {
     buildResources: "resources",
     output: "dist",
   },
   linux: {
-    target: ["AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip"],
+    target: [ "AppImage", "snap", "deb", "pacman", "rpm", "freebsd", "zip" ],
     executableName: "AndroidMessages",
-    executableArgs: [
-      "--ozone-platform-hint=auto",
-      "--enable-features=WaylandWindowDecorations",
-    ],
+    executableArgs:
+        [
+          "--ozone-platform-hint=auto",
+          "--enable-features=WaylandWindowDecorations",
+        ],
     category: "Internet",
   },
   win: {
-    target: ["nsis", "portable"],
+    target: [ "nsis", "portable" ],
   },
   mac: {
     category: "public.app-category.social-networking",
-    target: { target: "default", arch: "universal" },
+    target: {target: "default", arch: "universal"},
   },
   portable: {
     artifactName: "${productName}-v${version}-${os}-${arch}.portable.${ext}",
