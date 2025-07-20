2dc36559efb07342769f0ca3cd9fae635fb3bf15
OrangeDrangon <25403517+OrangeDrangon@users.noreply.github.com>
2025-07-20 15:55:38 -0400

    update dependencies and fix up devenv

--- devenv.nix
+++ devenv.nix
@@ -3,8 +3,11 @@
 {
   packages = [ 
     pkgs.gitFull
+    pkgs.nixd
   ];
 
+  languages.nix.enable = true;
+  
   languages.javascript.enable = true;
   languages.javascript.yarn.enable = true;
   languages.javascript.yarn.install.enable = true;
